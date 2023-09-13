fd11b3304cd4e5f78ea8c0a2e9c2d1e97d4b8953
peixoto <peixoto@pop-os.localdomain>
2023-09-13 09:38:10 -0300

    login added

--- bookwise/user/views.py
+++ bookwise/user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Wrong password or username')
+
+    return render(request, "login.html")
