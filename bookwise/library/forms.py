ba5fca2dc09bb4becfd6720ee83a69486a88f8b9
peixoto <peixoto@pop-os.localdomain>
2023-09-26 16:08:49 -0300

    style updated

--- bookwise/library/forms.py
+++ bookwise/library/forms.py
@@ -2,6 +2,17 @@ from django import forms
 from .models import Library
 
 class LibraryForm(forms.ModelForm):
+    name = forms.CharField(
+        required=True,
+        max_length=100,
+        label="Name",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+    
     class Meta:
         model = Library
         fields = ('name',)
